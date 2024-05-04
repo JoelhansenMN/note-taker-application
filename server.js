a136f55c81104ad39ff5a370a99841f5b6c78dd5
JoelhansenMN <joelhansen22@gmail.com>
2024-05-04 17:47:27 -0500

    initial commit...i forgot to do this first again

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path')
+const { v4: uuidv4 } = require('uuid');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+//sets up express app to handle data parser/middleware created
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//express to create a route for every file in the public folder
+app.use(express.static('public'));
+
+//get route that should read the db.json file and return the notes as JSON
+app.get('/api/notes', (req,res) =>{
+  //res.sendFile(path.join(__dirname, '../db/db.json'))
+  let allNotes = fs.readFileSync(path.join(__dirname, './db/db.json'))
+  allNotes = JSON.parse(allNotes)
+  res.json(allNotes)
+});
+
+//Post route
+app.post('/api/notes', (req,res)=> {
+  let allNotes = fs.readFileSync(path.join(__dirname, './db/db.json'))
+  allNotes = JSON.parse(allNotes)
+  allNotes.push(req.body)
+  fs.writeFileSync(path.join(__dirname, "./db/db.json"), JSON.stringify(allNotes))
+  allNotes = JSON.parse(allNotes)
+  res.json(allNotes)
+});
+
+//get routes for for notes.html file
+app.get('/notes', (req,res) =>{
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+});
+
+app.get('/', (req, res) =>{
+  res.sendFile(path.join(__dirname, './public/index.html'))
+});
+
+//get route for the index.html file
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname, './public/index.html'))
+});
+
+
+//app listener to initialize the server
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
